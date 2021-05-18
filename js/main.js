3a585ca247018e3ba58ba7b3e513fd89284e5e6c
jcontreras5 <johnny_paze@yahoo.com>
2021-05-18 15:01:15 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,47 @@
+//==================Exercise #1 ==========//
+/*Write a function that takes in the string and the list of dog names, loops through 
+the list and checks that the current name is in the string passed in. The output should be:
+"Matched dog_name" if name is in the string, if no matches are present console.log "No Matches"
+*/
+let dog_string = "Hello Max, my name is Dog, and I have purple eyes!"
+let dog_names = ["Max","HAS","PuRple","dog"]
+
+function findWords(list,target){
+    //Your code goes here
+    for(let i=0; i<list.length; i++){
+        let lower=target.toLowerCase()
+        let current=list[i].toLowerCase
+        if (lower.search(current) != -1){
+            return `${list[i]} is a match `
+        } else {
+            return `${list[i]} has no match `
+        }
+    }
+}
+
+console.log(findWords(dog_names,dog_string))
+
+//Call method here with parameters
+
+//============Exercise #2 ============//
+/*Write a fucntion that takes in an array and removes every even index with a splice,
+and replaces it with the string "even index" */
+
+// Given arr == ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
+
+function replaceEvens(arr){
+    for(let i=0;i<arr.length;i++){
+        if (i != 0 && i%2==0){
+            arr.splice(i,1,'even index')
+        }else{
+            continue
+        }
+    }
+    return arr
+}
+
+console.log(replaceEvens(["Max","Baseball","Reboot","Goku","Trucks","Rodger"]))
+
+//Expected output
+//Given arr == ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
+//Output arr == ["even index","Baseball","even index","Goku","even index","Rodger"]
